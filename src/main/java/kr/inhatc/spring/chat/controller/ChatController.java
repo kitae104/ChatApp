c9dba38292f72c8fd110b0ce9ae26a6ccd1dc4f1
Kitae's Github <aqua0405@gmail.com>
2022-11-10 00:25:38 +0900

    채팅 디비 설정 및 컨트롤로 설정

---
+++ src/main/java/kr/inhatc/spring/chat/controller/ChatController.java
@@ -0,0 +1,37 @@
+package kr.inhatc.spring.chat.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.inhatc.spring.chat.entity.Chat;
+import kr.inhatc.spring.chat.repository.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController
+{
+	private final ChatRepository chatRepository;
+	
+	// 데이터가 생길 때 마다 지속적으로 처리 
+	@GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
+		return chatRepository.mFindBySender(sender, receiver)
+				.subscribeOn(Schedulers.boundedElastic());
+	}
+	
+	@PostMapping("/chat")
+	public Mono<Chat> setMsg(@RequestBody Chat chat){
+		chat.setCreatedAt(LocalDateTime.now());
+		return chatRepository.save(chat);
+	}
+}
---
+++ src/main/java/kr/inhatc/spring/chat/entity/Chat.java
@@ -0,0 +1,24 @@
+package kr.inhatc.spring.chat.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "chat")
+public class Chat
+{
+	@Id
+	private String id; 
+	
+	private String msg;
+	
+	private String sender;
+	
+	private String receiver;
+	
+	private LocalDateTime createdAt;
+}
